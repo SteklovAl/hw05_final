50e77a78c107bacfe60bed76a7d7377df3ec9830
unknown <stelkaleksandr@yandex.ru>
2022-10-26 14:42:33 +0500

    статика

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -19,6 +19,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    'www.SteklovAL.pythonanywhere.com',
+    'SteklovAL.pythonanywhere.com',
 ]
 
 
@@ -36,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -46,6 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
